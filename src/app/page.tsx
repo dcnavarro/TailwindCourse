c20b14cd8fe5fd314fd217a3d120086b1a159825
Débora Navarro <navarrodeboracecilia@gmail.com>
2023-11-25 06:04:01 -0300

    customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <div>
+    <h1 className="text-3xl sm:text-base md:mb-xl md:text-lg lg:text-3xl text-mypink font-bold underline bg-gray-50 border-8 border-rose-800">
       Hello world!
-    </h1>
+      </h1>
+      <h2 className="text-yellow-600 text-2xl sm:text-base md:text-lg lg:text-3xl">This is yellow color</h2>
+      <h3 className="text-xl">This is an h3</h3>
+      <p className="text-base">This is a paragraph</p>
+      <p className="text-xs note">This is a note</p>
+      </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,29 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
+    spacing: {
+      sm: "16px",
+      md: "24px",
+      lg: "32px",
+      xl: "48px",
+    },
+    extend: {
+      colors: {
+        mypink: "#D61596",
+      },
+    },
   },
   plugins: [],
 };
